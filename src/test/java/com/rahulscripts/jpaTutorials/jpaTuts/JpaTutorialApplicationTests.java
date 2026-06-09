4065b574d839399b5840718b2cf5b60c329120d9
rahul pareek <itsrahul615@gmail.com>
2026-06-09 23:28:46 +0530

    Practicing Spring Data JPA in detail

---
+++ src/test/java/com/rahulscripts/jpaTutorials/jpaTuts/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulscripts.jpaTutorials.jpaTuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
